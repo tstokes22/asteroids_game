dfccb5ac2a387b1308d1acf30ede6c411d61957b
tstokes22 <tbstokes22@gmail.com>
2025-06-20 13:21:44 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+
+def main():
+    # Initialize all imported pygame modules
+    pygame.init()
+    
+    # Print statements
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while(running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
